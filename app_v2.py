fe2741c8e2f44f8e43374d03b8a35182ef2bd504
Xiaochen Wang <xiaochen@sensorscall.com>
2019-05-08 17:34:01 -0400

    turn print sentence

--- app_v2.py
+++ app_v2.py
@@ -55,6 +55,7 @@ class stun_turn:
             else:
                 # forward symmetric chat msg, act as TURN server
                 try:
+                    print(data)
                     socket_turn.sendto(data, symmetric_chat_clients[addr])
                 except KeyError:
                     socket_turn.sendto("LC Stop\0", addr)
