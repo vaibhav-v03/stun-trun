e9e301e1b63e28fa158a4c8bc7d19e0f5b7e91a4
Xiaochen Wang <xiaochen@sensorscall.com>
2019-06-26 10:50:17 -0400

    bug fixing

--- app_v2.py
+++ app_v2.py
@@ -157,7 +157,7 @@ class stun_turn:
                             # pool created ==> device is occupied, decline another app's request
                             if device_type == '2':
                                 print("device occupied")
-                                sockfd.send("occupied", addr)
+                                sockfd.sendto("occupied", addr)
                                 continue
 
                             if nat_type_id != '0' or self.symmetric_chat_clients[pool][0] != '0':
